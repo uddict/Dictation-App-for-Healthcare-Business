4c46d0c9c2a870fa78888706dec9309f9d300d61
rupesh <rpiwal02@gmail.com>
2024-09-11 22:28:25 +0530

    implemented routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import Dictate from "./pages/Dictate";
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<Dictate />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
