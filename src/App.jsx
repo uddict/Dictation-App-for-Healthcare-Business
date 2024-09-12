bc1fa477bb7cf022914a4d517bc5982749f295ac
rupesh <rpiwal02@gmail.com>
2024-09-12 14:23:27 +0530

    added new routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Dictate from "./pages/Dictate";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Progress from "./pages/Progress";
+import Soap from "./pages/Soap";
 
 const App = () => {
   return (
@@ -9,6 +11,8 @@ const App = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Dictate />} />
+        <Route path="/progress" element={<Progress />} />
+        <Route path="/soap" element={<Soap />} />
         {/* <Route path="/about" element={<About />} /> */}
       </Routes>
     </>
