85cbb9fc33d6889dc57bdaa4da3c4cb256011c7c
rupesh <rpiwal02@gmail.com>
2024-09-19 22:50:14 +0530

    changed heading to MediScribe

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <div className="bg-gradient-to-r from-[#7dbeff] to-[#6781ff] shadow-md text-[#ffffff] py-4 text-center">
-      <h1 className="text-2xl font-semibold">Dictation HealthCare</h1>
+      <h1 className="text-2xl font-semibold">MEDISCRIBE</h1>
     </div>
   );
 };
