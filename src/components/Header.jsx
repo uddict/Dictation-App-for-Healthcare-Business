c3138b819ddb40b696567056f30a44150ad1df5a
rupesh <rpiwal02@gmail.com>
2024-10-04 11:11:47 +0530

    changed header text to react-router Link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="bg-gradient-to-r from-[#7dbeff] to-[#6781ff] shadow-md text-[#ffffff] py-4 text-center">
-      <h1 className="text-2xl font-semibold">MEDISCRIBE</h1>
+      <Link className="text-2xl font-semibold">MEDISCRIBE</Link>
     </div>
   );
 };
