a27f7c422ecd8dd8d446b2bce22556a46702eb43
Uddict <uditkp024@gmail.com>
2024-10-14 21:36:32 +0530

    Login Page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { Link as RouterLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="bg-gradient-to-r from-[#7dbeff] to-[#6781ff] shadow-md text-[#ffffff] py-4 text-center">
-      <h1 className="text-2xl font-semibold">MEDISCRIBE</h1>
+    <div className="bg-gradient-to-r from-[#7dbeff] to-[#6781ff] shadow-md text-[#ffffff] py-4 px-6 flex justify-between items-center">
+      <div className="w-24">{/*maybe for adding a logo*/}</div>
+      <h1 className="text-2xl font-semibold flex-grow text-center">
+        MEDISCRIBE
+      </h1>
+      <RouterLink to="/login" className="text-white hover:text-gray-200">
+        <button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Login
+        </button>
+      </RouterLink>
     </div>
   );
 };
